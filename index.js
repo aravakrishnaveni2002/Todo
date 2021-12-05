aa64b76aa67930cc19c89bc7cd60d1bff3e1fd0e
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2021-12-05 16:04:01 +0530

    created router controller and views

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
+app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error on running the server: ${err}`);
